24d83a068a25c5501999daa30e462d2ff8583241
Kohyuckjoon <akhj123akhj@naver.com>
2025-09-16 18:21:39 +0900

    2025.09.16 Yuscon Disto App - 프로젝트 생성, 선택 화면 Fragment 전환 추가

--- app/src/main/java/com/example/yscdisto/ui/MainActivity.kt
+++ app/src/main/java/com/example/yscdisto/ui/MainActivity.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import com.example.yscdisto.CameraSearchListFragment
+import com.example.yscdisto.ProjectCreateFragment
+import com.example.yscdisto.ProjectSelectFragment
 import com.example.yscdisto.ui.disto.DistoConnectFragment
 import com.example.yscdisto.R
 import com.example.yscdisto.ui.disto.StartMeasurementFragment
@@ -12,42 +15,54 @@ import com.example.yscdisto.databinding.ActivityMainBinding
 import com.example.yscdisto.ui.disto.MeasurementListFragment
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var dataBinding : ActivityMainBinding
+    private lateinit var viewBinding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        dataBinding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(dataBinding.root)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
 
-        dataBinding.mcDistioConn.setOnClickListener {
-//            val intent = Intent(this, DistoConnection::class.java)
-//            startActivity(intent)
+
+
+        viewBinding.mcDistioConn.setOnClickListener {
             supportFragmentManager.commit {
                 replace(R.id.fl_disto_conn, DistoConnectFragment())
                 addToBackStack(null)
             }
         }
 
-        dataBinding.mcCameraConn.setOnClickListener {
+        viewBinding.mcCameraConn.setOnClickListener {
             supportFragmentManager.commit {
                 replace(R.id.fl_camera_conn, CameraSearchListFragment())
                 addToBackStack(null)
             }
         }
 
-        dataBinding.mcMeasurementList.setOnClickListener {
+        viewBinding.mcMeasurementList.setOnClickListener {
             supportFragmentManager.commit {
                 replace(R.id.fl_measurement_list, MeasurementListFragment())
                 addToBackStack(null)
             }
         }
 
-        dataBinding.mcMeasurement.setOnClickListener {
+        viewBinding.mcMeasurement.setOnClickListener {
             supportFragmentManager.commit {
                 replace(R.id.fl_start_measurement, StartMeasurementFragment())
                 addToBackStack(null)
                 Log.e("khj", "test_01");
+            }
+        }
+
+        viewBinding.mcProjectCreate.setOnClickListener {
+            supportFragmentManager.commit {
+                replace(R.id.fl_project_create, ProjectCreateFragment())
+            }
+        }
+
+        viewBinding.mcProjectSelect.setOnClickListener {
+            supportFragmentManager.commit {
+                replace(R.id.fl_project_select, ProjectSelectFragment())
             }
         }
     }
