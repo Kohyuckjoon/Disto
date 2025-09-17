f6c2863c5026b1aa4a14dfbe31ad035d6bc5f68e
Kohyuckjoon <akhj123akhj@naver.com>
2025-09-17 16:53:58 +0900

    2025.09.17 Yuscon Disto App - 프로젝트 생성 화면에서 생성 버튼 클릭시 다른 화면으로 이동되는 문제 해결 / 포커스 문제로 처리

--- app/src/main/java/com/example/yscdisto/ui/MainActivity.kt
+++ app/src/main/java/com/example/yscdisto/ui/MainActivity.kt
@@ -4,10 +4,9 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
-import androidx.fragment.app.replace
-import com.example.yscdisto.CameraSearchListFragment
-import com.example.yscdisto.ProjectCreateFragment
-import com.example.yscdisto.ProjectSelectFragment
+import com.example.yscdisto.ui.disto.CameraSearchListFragment
+import com.example.yscdisto.ui.disto.ProjectCreateFragment
+import com.example.yscdisto.ui.disto.ProjectSelectFragment
 import com.example.yscdisto.ui.disto.DistoConnectFragment
 import com.example.yscdisto.R
 import com.example.yscdisto.ui.disto.StartMeasurementFragment
@@ -24,6 +23,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(viewBinding.root)
 
 
+        viewBinding.mcProjectCreate.setOnClickListener {
+            supportFragmentManager.commit {
+                replace(R.id.fl_project_create, ProjectCreateFragment())
+                addToBackStack(null)
+            }
+        }
+
+        viewBinding.mcProjectSelect.setOnClickListener {
+            supportFragmentManager.commit {
+                replace(R.id.fl_project_select, ProjectSelectFragment())
+                addToBackStack(null)
+            }
+        }
 
         viewBinding.mcDistioConn.setOnClickListener {
             supportFragmentManager.commit {
@@ -51,18 +63,6 @@ class MainActivity : AppCompatActivity() {
                 replace(R.id.fl_start_measurement, StartMeasurementFragment())
                 addToBackStack(null)
                 Log.e("khj", "test_01");
-            }
-        }
-
-        viewBinding.mcProjectCreate.setOnClickListener {
-            supportFragmentManager.commit {
-                replace(R.id.fl_project_create, ProjectCreateFragment())
-            }
-        }
-
-        viewBinding.mcProjectSelect.setOnClickListener {
-            supportFragmentManager.commit {
-                replace(R.id.fl_project_select, ProjectSelectFragment())
             }
         }
     }
--- app/src/main/java/com/example/yscdisto/ui/disto/ProjectSelectFragment.kt
+++ app/src/main/java/com/example/yscdisto/ui/disto/ProjectSelectFragment.kt
@@ -1,10 +1,11 @@
-package com.example.yscdisto
+package com.example.yscdisto.ui.disto
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.yscdisto.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
